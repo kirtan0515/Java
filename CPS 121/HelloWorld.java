b6e5c4d8030c49f234b4b27bf7f3902de6080819
kirtan0515 <kirtan.patel0515@gmail.com>
2022-01-27 19:24:02 -0500

    HelloWorld.java

---
+++ CPS 121/HelloWorld.java
@@ -0,0 +1,14 @@
+/**************************
+ * Name : Kirtankumar Patel
+ * Date : January 27, 2022
+ * Prof : Dr. Rodney Clark
+ * Desc : Hello World
+***************************/
+
+public class HelloWorld {
+    // Program begins with main() method
+    public static void main(String[] args){
+        System.out.println("Hello, World!"); // prints Hello, World!
+        System.out.println("This is my first program!"); //print message
+    }
+}
