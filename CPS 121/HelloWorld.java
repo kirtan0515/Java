d47675b73d29b685407f9714b485e7fa2fab082c
kirtan0515 <kirtan.patel0515@gmail.com>
2022-01-27 19:42:56 -0500

    HelloWorld.java Added More Sample Output

--- CPS 121/HelloWorld.java
+++ CPS 121/HelloWorld.java
@@ -1,3 +1,5 @@
+import javax.crypto.spec.PSource;
+
 /**************************
  * Name : Kirtankumar Patel
  * Date : January 27, 2022
@@ -8,7 +10,14 @@
 public class HelloWorld {
     // Program begins with main() method
     public static void main(String[] args){
+        System.out.println("----SAMPLE #1 ---------");
         System.out.println("Hello, World!"); // prints Hello, World!
         System.out.println("This is my first program!"); //print message
+        System.out.println("\n----SAMPLE #2 ---------");
+        System.out.println("Hello\nHello");//print Hello! Hello!
+        System.out.println("\n----SAMPLE #3----------");
+        System.out.println("Hello! Hello!\nThis is my first Program!");//print Hello! Hello! This is my first Program!
+
+
     }
 }
