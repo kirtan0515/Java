2a14096318d86807de10998c5181d3cc5dbf4dc4
kirtan0515 <kirtan.patel0515@gmail.com>
2022-02-07 20:56:20 -0500

    Welcome.java added

---
+++ CPS 121/Welcome.java
@@ -0,0 +1,18 @@
+/**************************
+ * Name : Kirtankumar Patel
+ * Date : February 7, 2022
+ * Prof : Dr. Rodney Clark
+ * Desc : Welcome
+ ***************************/
+public class Welcome {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Java Programming");
+        System.out.print("I think this class ");
+        System.out.println("will be awesome!!");
+        System.out.println("I promise not to cheat in this class");
+        System.out.println("If caught, I will be withdrawn with an F grade");
+        System.out.println("Como estas hoy");
+        System.out.println("J’espere que tu aimes ce cours");
+        System.out.println("Buona gironata");
+    }
+}
