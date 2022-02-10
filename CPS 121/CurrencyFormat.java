e773873ec14376fd58590e9d20fe614d9937ae7d
kirtan0515 <kirtan.patel0515@gmail.com>
2022-02-10 13:18:52 -0500

    Columns.java

---
+++ CPS 121/CurrencyFormat.java
@@ -0,0 +1,18 @@
+/**
+ This program demonstrates how to use the System.out.printf
+ method to format a number as currency.
+ */
+
+public class CurrencyFormat
+{
+    public static void main(String[] args)
+    {
+        //Declare variables
+        double monthlyPay = 5000.0;
+        double annualPay = monthlyPay * 12;
+
+        System.out.printf("Your annual pay is ($%,.2f)\n", annualPay);
+
+        //Display output
+    }
+}
