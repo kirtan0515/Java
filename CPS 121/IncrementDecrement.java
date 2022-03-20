32f096202f254f44eaa3ce485947f79eefcb95a5
kirtan0515 <kirtan.patel0515@gmail.com>
2022-03-19 21:24:20 -0400

    IncrementDecrement

---
+++ CPS 121/IncrementDecrement.java
@@ -0,0 +1,33 @@
+/**************************
+ * Name : Kirtankumar Patel
+ * Date : March 19,2022
+ * Prof : Dr. Rodney Clark
+ * Desc : Code listing 4-1
+ ***************************/
+/**
+ * This program demonstrates the ++ and −− operators.
+ */
+
+public class IncrementDecrement {
+    public static void main(String[] args)
+    {
+        int number = 4; // number starts out with 4
+
+        // Display the value in number.
+        System.out.println("number is " + number);
+        System.out.println("I will increment number.");
+
+        // Increment number.
+        number++;
+
+        // Display the value in number again.
+        System.out.println("Now, number is " + number);
+        System.out.println("I will decrement number.");
+
+        // Decrement number.
+        number--;
+
+        // Display the value in number once more.
+        System.out.println("Now, number is " + number);
+    }
+}
