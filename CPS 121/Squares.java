86241ff0a71015ca99696a552d64e0a00681120c
kirtan0515 <kirtan.patel0515@gmail.com>
2022-03-19 21:58:41 -0400

    Squares

---
+++ CPS 121/Squares.java
@@ -0,0 +1,22 @@
+/**************************
+ * Name : Kirtankumar Patel
+ * Date : March 19,2022
+ * Prof : Dr. Rodney Clark
+ * Desc : Code listing 4-7
+ ***************************/
+public class Squares
+{
+    public static void main(String[] args)
+    {
+        int number;  // Loop control vriable
+
+        System.out.println("Number Number Squared");
+        System.out.println("-----------------------");
+
+        for (number = 1; number <= 10; number++)
+        {
+            System.out.println(number + "\t\t" + number * number);
+        }
+
+    }
+}
